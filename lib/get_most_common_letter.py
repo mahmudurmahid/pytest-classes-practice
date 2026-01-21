4af82a34a86cb61ebf706944d5638e53747db970
mahmudur mahid <m.mahid@icloud.com>
2026-01-21 19:31:35 +0000

    debugged get_most_common_letter function

--- lib/get_most_common_letter.py
+++ lib/get_most_common_letter.py
@@ -1,8 +1,11 @@
 def get_most_common_letter(text):
     counter = {}
+
     for char in text:
-        counter[char] = counter.get(char, 0) + 1
-    letter = sorted(counter.items(), key=lambda item: item[1])[0][1]
+        if char.isalpha():
+            counter[char] = counter.get(char, 0) + 1
+
+    letter = max(counter.items(), key=lambda item: item[1])[0]
     return letter
 
 
