cbd9b35cb8642706ac3cf3c1b46425d342226bf2
mahmudur mahid <m.mahid@icloud.com>
2026-01-21 19:06:33 +0000

    debug factorial method to provide correct answer

--- lib/factorial.py
+++ lib/factorial.py
@@ -1,11 +1,9 @@
 def factorial(n):
     product = 1
-    print(f"at the start product is {product}")
-    while n > 0:
-        n -= 1
-        print(f"we multiply {product} by {n}")
+    
+    while n > 1:
         product *= n
-        print(f"we get {product}")
+        n -= 1
 
     return product
 
