170e3123acbc5f35b0b37457c248005e5f369559
mahmudur mahid <m.mahid@icloud.com>
2026-01-22 13:07:37 +0000

    added test_diary method to see if format returns correct output

--- tests/test_diary_entry.py
+++ tests/test_diary_entry.py
@@ -1,2 +1,12 @@
 from lib.diary_entry import DiaryEntry
 
+"""
+format method: when the arguments title and contents are provided,
+the method returns a correctly formatted diary entry string
+"""
+def test_diary_entry_format_returns_formatted_title_and_contents():
+    diary_entry = DiaryEntry("My Title", "These are the contents")
+
+    result = diary_entry.format()
+    assert result == "My Title: These are the contents"
+
