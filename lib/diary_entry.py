a5536446726999f5c6953b6b5e27d60ea388b2da
mahmudur mahid <m.mahid@icloud.com>
2026-01-22 14:57:37 +0000

    added reading chunk method logic

--- lib/diary_entry.py
+++ lib/diary_entry.py
@@ -19,23 +19,12 @@ class DiaryEntry:
         return f"{time} mins"
 
     def reading_chunk(self, wpm, minutes):
-        # Parameters
-        #   wpm: an integer representing the number of words the user can read
-        #        per minute
-        #   minutes: an integer representing the number of minutes the user has
-        #            to read
-        # Returns:
-        #   string: a chunk of the contents that the user could read in the
-        #           given number of minutes
-        #
-        # If called again, `reading_chunk` should return the next chunk,
-        # skipping what has already been read, until the contents is fully read.
-        # The next call after that should restart from the beginning.
-        pass
-
+        number_of_words = wpm * minutes
+        return " ".join(self.contents.split()[:number_of_words])
 
 result = DiaryEntry("My Title", "These are the contents")
 format = result.format()
 count_words = result.count_words()
 reading_time = result.reading_time(2)
-print()
+reading_chunk = result.reading_chunk(2, 2)
+print(reading_chunk)
