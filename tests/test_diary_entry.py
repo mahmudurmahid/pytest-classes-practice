6100deeca48e9155257fde5cd0ef4c77ec0c4ef2
mahmudur mahid <m.mahid@icloud.com>
2026-01-22 14:00:23 +0000

    added test_diary count_words method to see if correct number of words provided

--- tests/test_diary_entry.py
+++ tests/test_diary_entry.py
@@ -10,3 +10,12 @@ def test_diary_entry_format_returns_formatted_title_and_contents():
     result = diary_entry.format()
     assert result == "My Title: These are the contents"
 
+"""
+count_words method: when the arguments title and contents are provided,
+the method returns the number of words used
+"""
+def test_diary_entry_count_words_returns_number_of_words():
+    diary_entry = DiaryEntry("My Title", "These are the contents")
+
+    result = diary_entry.count_words()
+    assert result == 6
