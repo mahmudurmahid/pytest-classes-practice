82a9369dd6a5e552c5fcffb25a01cccceadb1065
mahmudur mahid <m.mahid@icloud.com>
2026-02-01 10:57:48 +0000

    copied class interface from md file to tesk tracker class file

--- lib/task_tracker.py
+++ lib/task_tracker.py
@@ -1,3 +1,23 @@
 class TaskTracker():
     def __init__(self):
-        pass
+        # parameters: none
+        # side effects: initialises an empty list of tasks 
+        pass # No code here yet
+
+    def add_task(self, task):
+        # parameters: (str) user adds a single task they want to complete later
+        # returns: nothing
+        # side effects: adds task to a list of tasks
+        pass # No code here yet
+    
+    def complete_task(self, task):
+        # parameters: (str) user completes a task
+        # returns: completed task message or list of tasks still to do
+        # side effects: removes task from list of tasks
+        pass # No code here yet
+    
+    def task_list(self):
+       # parameters: none
+       # returns: (list) tasks yet to be completed
+       # side effects: none
+       pass # No code here yet
