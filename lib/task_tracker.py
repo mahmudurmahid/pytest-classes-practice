2e1f8cb6f75b56c860d38c8ae0abc53faa02e166
mahmudur mahid <m.mahid@icloud.com>
2026-02-01 16:27:24 +0000

    complete logic for task tracker class

--- lib/task_tracker.py
+++ lib/task_tracker.py
@@ -1,23 +1,30 @@
-class TaskTracker():
+class TaskTracker:
     def __init__(self):
         # parameters: none
         # side effects: initialises an empty list of tasks 
-        pass # No code here yet
+        self.tasks = []
 
     def add_task(self, task):
         # parameters: (str) user adds a single task they want to complete later
         # returns: nothing
         # side effects: adds task to a list of tasks
-        pass # No code here yet
+        self.tasks.append(task)
     
     def complete_task(self, task):
         # parameters: (str) user completes a task
         # returns: completed task message or list of tasks still to do
         # side effects: removes task from list of tasks
-        pass # No code here yet
+        if task not in self.tasks:
+            raise ValueError(f"Task not found: {task}")
+        
+        self.tasks.remove(task)
+
+        if len(self.tasks) == 0:
+            return f"Task completed: {task}"
+        return self.tasks
     
     def task_list(self):
-       # parameters: none
-       # returns: (list) tasks yet to be completed
-       # side effects: none
-       pass # No code here yet
+        # parameters: none
+        # returns: (list) tasks yet to be completed
+        # side effects: none
+        return self.tasks
