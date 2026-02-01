719a6e30bf23cda2d4e83dc30fbeab7cf47e1742
mahmudur mahid <m.mahid@icloud.com>
2026-02-01 16:28:17 +0000

    removed comments

--- lib/task_tracker.py
+++ lib/task_tracker.py
@@ -1,19 +1,11 @@
 class TaskTracker:
     def __init__(self):
-        # parameters: none
-        # side effects: initialises an empty list of tasks 
         self.tasks = []
 
     def add_task(self, task):
-        # parameters: (str) user adds a single task they want to complete later
-        # returns: nothing
-        # side effects: adds task to a list of tasks
         self.tasks.append(task)
     
     def complete_task(self, task):
-        # parameters: (str) user completes a task
-        # returns: completed task message or list of tasks still to do
-        # side effects: removes task from list of tasks
         if task not in self.tasks:
             raise ValueError(f"Task not found: {task}")
         
@@ -24,7 +16,4 @@ class TaskTracker:
         return self.tasks
     
     def task_list(self):
-        # parameters: none
-        # returns: (list) tasks yet to be completed
-        # side effects: none
         return self.tasks
